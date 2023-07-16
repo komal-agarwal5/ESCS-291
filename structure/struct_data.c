d51037a3c764f4bd0b75ba79894094493f61fb47
Komal Agarwal <122633300+komal-agarwal5@users.noreply.github.com>
2023-07-16 22:11:23 +0530

    Update struct_data.c

--- structure/struct_data.c
+++ structure/struct_data.c
@@ -21,11 +21,11 @@ void main()
 		printf("Enter roll : ");
 		scanf("%d",&st[i].roll);
 		sum=0;
-		printf("Enter subject code1 and marks : ");
+		printf("Enter subject code 1 and marks : ");
 		scanf("%s%d",st[i].code1,&st[i].marks1);
-		printf("Enter subject code2 and marks : ");
+		printf("Enter subject code 2 and marks : ");
 		scanf("%s%d",st[i].code2,&st[i].marks2);
-		printf("Enter subject code3 and marks : ");
+		printf("Enter subject code 3 and marks : ");
 		scanf("%s%d",st[i].code3,&st[i].marks3);
 		sum+=st[i].marks1+st[i].marks2+st[i].marks3;
 		st[i].avg=sum/3;
@@ -34,9 +34,9 @@ void main()
 	{
 		printf("\nCourse Name : %s",st[i].name);
 		printf("\nRoll : %d",st[i].roll);
-		printf("\nSubject code1 and marks : %s  %d",st[i].code1,st[i].marks1);
-		printf("\nSubject code2 and marks : %s  %d",st[i].code2,st[i].marks2);
-		printf("\nSubject code3 and marks : %s  %d",st[i].code3,st[i].marks3);
+		printf("\nSubject code 1 and marks : %s  %d",st[i].code1,st[i].marks1);
+		printf("\nSubject code 2 and marks : %s  %d",st[i].code2,st[i].marks2);
+		printf("\nSubject code 3 and marks : %s  %d",st[i].code3,st[i].marks3);
 		printf("\nAverage marks : %f\n",st[i].avg);
 	}
 }
